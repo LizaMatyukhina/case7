c2e3bedd41c0aa1a910c56a3a0bb4f0ebb7d5680
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2018-12-11 17:29:57 +0700

    start

---
+++ case7.py
@@ -0,0 +1,17 @@
+"""
+Case #7
+
+Developers:
+- Aigerim Rashidova
+- Alina Romanenko
+- Elizaveta Matyukhina.
+"""
+
+# Aigerim - work with file and text.
+
+
+# Alina - work with words and dictionaries.
+
+
+# Liza - work with dictionaries, sentences and random.
+
