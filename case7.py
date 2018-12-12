3f77624872f4f1fa76e72ecacf60f3903e16c87d
aigerimka <aigerish0603@gmail.com>
2018-12-13 02:58:31 +0700

    replacing and deleting the symbols

--- case7.py
+++ case7.py
@@ -1,6 +1,5 @@
 """
-Case #7
-
+Case-study #7 Generation of sentences
 Developers:
 - Aigerim Rashidova
 - Alina Romanenko
@@ -8,21 +7,30 @@ Developers:
 """
 
 # Aigerim - work with file and text.
-with open('input.txt') as f_in:
+with open('input.txt', encoding="UTF-8") as f_in:
     text = f_in.read()
-symbols_of_end = '.!?'
-for i in symbols_of_end:
-    if i in text:
-        text = text.replace(i, ' ')
-symbols = '"#$%&\'()*+-/:;<=>@[]^_`{|}~'
-for i in symbols:
-    if i in text:
-        text.replace(i, '')
-punc = '.,!?'
-for i in punc:
-    a = ' i'
-    if a in text:
-        text.replace(a, i)
+with open('output.txt', 'w', encoding="UTF-8") as f_out:
+    symbols_of_end = ['.', '!', '?']
+    for i in symbols_of_end:
+        if i in text:
+            text = text.replace(i, ' ')
+    symbols = ['"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '/', ':', ';', '<', '=', '>', '@', '[', ']', '^', '_',
+               '`', '{', '|', '}', '~', '—', '«', '»']
+    for i in symbols:
+        if i in text:
+            text = text.replace(i, '')
+    punctuation = [' .', ' ,', ' !', ' ?']
+    for i in punctuation:
+        if i in text:
+            if i == ' .':
+                text = text.replace(i, '.')
+            elif i == ' ,':
+                text = text.replace(i, ',')
+            elif i == ' !':
+                text = text.replace(i, '!')
+            elif i == ' ?':
+                text = text.replace(i, '?')
+    f_out.write(text)
 
 # Alina - work with words and dictionaries.
 
