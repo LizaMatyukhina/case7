55ec32d83521ffd9c927a85c79bcff09444bd8c4
aigerimka <aigerish0603@gmail.com>
2018-12-12 10:31:22 +0700

    work with the text

--- case7.py
+++ case7.py
@@ -8,7 +8,21 @@ Developers:
 """
 
 # Aigerim - work with file and text.
-
+with open('input.txt') as f_in:
+    text = f_in.read()
+symbols_of_end = '.!?'
+for i in symbols_of_end:
+    if i in text:
+        text = text.replace(i, ' ')
+symbols = '"#$%&\'()*+-/:;<=>@[]^_`{|}~'
+for i in symbols:
+    if i in text:
+        text.replace(i, '')
+punc = '.,!?'
+for i in punc:
+    a = ' i'
+    if a in text:
+        text.replace(a, i)
 
 # Alina - work with words and dictionaries.
 
