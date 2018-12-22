de494affc7756e3e7a7540a09c657e9f8c58be38
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2018-12-22 23:14:32 +0700

    random, last edition!!!

--- case7.py
+++ case7.py
@@ -11,23 +11,22 @@ import random
 file_in = input('Введите имя файла: ')
 with open(file_in, encoding="UTF-8") as f_in:
     text = f_in.read()
-    with open('output.txt', 'w', encoding="UTF-8") as f_out:
-        symbols = ['"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '/', ':', ';', '<', '=', '>', '@', '[', ']', '^', '_',
-               '`', '{', '|', '}', '~', '—', '«', '»']
-        for i in symbols:
-            if i in text:
-                text = text.replace(i, '')
-        punctuation = [' .', ' ,', ' !', ' ?']
-        for i in punctuation:
-            if i in text:
-                if i == ' ,':
-                    text = text.replace(i, ',')
-                elif i == ' .':
-                    text = text.replace(i, '.')
-                elif i == ' !':
-                    text = text.replace(i, '!')
-                elif i == ' ?':
-                    text = text.replace(i, '?')
+    symbols = ['"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '/', ':', ';', '<', '=', '>', '@', '[', ']', '^', '_',
+            '`', '{', '|', '}', '~', '—', '«', '»']
+    for i in symbols:
+        if i in text:
+            text = text.replace(i, '')
+    punctuation = [' .', ' ,', ' !', ' ?']
+    for i in punctuation:
+        if i in text:
+            if i == ' ,':
+                text = text.replace(i, ',')
+            elif i == ' .':
+                text = text.replace(i, '.')
+            elif i == ' !':
+                text = text.replace(i, '!')
+            elif i == ' ?':
+                text = text.replace(i, '?')
 
 startwords = []
 allwords = []
@@ -50,34 +49,41 @@ for i in range(len(lst)-1):
         endwords.append(word)
 
 number_of_sentences = int(input('Введите количество предложений: '))
-for i in range(number_of_sentences):
-    count = 0
-    sentence = []
-    first_word = random.choice(startwords)
-    while first_word[-1] == '.':
+with open('output.txt', 'w', encoding="UTF-8") as f_out:
+    for i in range(number_of_sentences):
+        count = 0
+        sentence = []
         first_word = random.choice(startwords)
-    sentence.append(first_word)
-    count += 1
-    while count < 20:
-        while count < 5:
-            word = random.choice(dict[sentence[-1]])
-            sentence.append(word)
-            if word[-1] == '.':
-                sentence.remove(word)
+        while first_word in endwords or len(dict[first_word]) < 2:
+            first_word = random.choice(startwords)
+        sentence.append(first_word)
+        count += 1
+        while count < 20:
+            while count < 5:
                 word = random.choice(dict[sentence[-1]])
                 sentence.append(word)
-            count += 1
-        word = random.choice(dict[sentence[-1]])
-        if word[-1] == '.':
-            sentence.append(word)
-            break
-        if count == 19 and word not in endwords:
-            if word[-1] != ',':
-                word = word + '.'
-            elif word[-1] == ',':
-                word.replace(',', '')
-        sentence.append(word)
-        count += 1
-    print(*sentence, sep=' ', end=' ')
+                if word in endwords:
+                    sentence.remove(word)
+                    word = random.choice(dict[sentence[-1]])
+                    sentence.append(word)
+                count += 1
+            if len(sentence) < 5 and sentence[-1][-1] == '.':
+                break
+            elif len(sentence) >= 5 and sentence[-1][-1] == '.':
+                print(*sentence, sep=' ', end=' ', file=f_out)
+                break
+            else:
+                word = random.choice(dict[sentence[-1]])
+                if word in endwords:
+                    sentence.append(word)
+                    break
+                if count == 19 and word not in endwords:
+                    if word[-1] != ',':
+                        word = word + '.'
+                    elif word[-1] == ',':
+                        word.replace(',', '')
+                sentence.append(word)
+                count += 1
+        print(*sentence, sep=' ', end=' ', file=f_out)
 
 
